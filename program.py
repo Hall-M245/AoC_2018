2da8284db77751305c681d72b9277155aabff74d
Matthew <Matthew.Hall@dal.ca>
2018-12-03 22:45:53 -0800

    Working on Day 04-01

--- program.py
+++ program.py
@@ -1,11 +1,13 @@
 import re
+from datetime import datetime
 
 def main():
     #day01_01()
     #day01_02()
     #day02_01()
     #day02_02()
-    day03_01()
+    #day03_01()
+    day04_01()
 
 def day01_01():
     freq = 0
@@ -90,15 +92,25 @@ def day03_01():
             claims.append(Claim(line))
             line = inFile.readline().strip()
     for c in claims:
-        for x in xrange(c.x, c.x+c.w):
-            for y in xrange(c.y, c.y+c.h):
+        for x in range(c.x, c.x+c.w):
+            for y in range(c.y, c.y+c.h):
                 fabric[x][y] += 1
-    for x in xrange(fabricW):
-        for y in xrange(fabricH):
+                if(fabric[x][y] > 1):
+                    c.intact = False
+    for x in range(fabricW):
+        for y in range(fabricH):
             if(fabric[x][y] > 1):
                 wastedSpace += 1
     print('Wasted fabric space: ' + str(wastedSpace))
-    print('Inact fabric piece ID: ' + str(id))
+    for c in claims:
+        if(c.intact == True):
+            for x in range(c.x, c.x+c.w):
+                for y in range(c.y, c.y+c.h):
+                    if(fabric[x][y] != 1):
+                        c.intact = False
+            if(c.intact == True):
+                print('Inact fabric piece ID: ' + str(c.id))
+
 
 class Claim:
 
@@ -112,6 +124,62 @@ class Claim:
         self.h = int(vals[4])
         self.intact = True
 
+def day04_01():
+    guards = {}
+    activities = []
+    with open('input/input_0401.txt', 'r') as inFile:
+        line = inFile.readline().strip()
+        while line:
+            parts = line.replace('[','').split('] ')
+            timeStamp = datetime.strptime(parts[0],'%Y-%m-%d %H:%M')
+            act = parts[1]
+            activities.append(Activity(act,timeStamp))
+            line = inFile.readline().strip()
+    activities.sort(key=lambda x: x.time)
+    for item in activities:
+        g = re.compile(r'\#\d{0,4}').findall(item.act)
+
+        if(len(g) > 0):
+            guardID = g[0].replace('#','')
+            if(guardID in guards):
+                currentGuard = guards[guardID]
+            else:
+                currentGuard = Guard(guardID)
+                guards[guardID] = currentGuard
+        elif(item.act == 'falls asleep'):
+            timeStart = item.time
+        elif(item.act == 'wakes up'):
+            timeEnd = item.time
+            sleepTime = int((timeEnd - timeStart).total_seconds()/60)
+            currentGuard.totalSleepTime += sleepTime
+            if(sleepTime > currentGuard.longestSleep):
+                currentGuard.longestSleep = sleepTime
+                currentGuard.longestMin = timeStart.minute
+    bestGuard = None
+    for g in guards:
+        currentGuard = guards[g]
+        if(bestGuard is None):
+            bestGuard = currentGuard
+        else:
+            if(currentGuard.totalSleepTime > bestGuard.totalSleepTime):
+                bestGuard = currentGuard
+    print('Guard ' + str(bestGuard.id) + ' was asleep for ' + str(bestGuard.totalSleepTime) + ' minutes, with the longest sleep starting at minute ' + str(bestGuard.longestMin) + '.')
+    print('Result: ' + str(int(bestGuard.id) * int(bestGuard.longestMin)))
+
+            
+class Guard:
+
+    def __init__(self, id):
+        self.id = id
+        self.totalSleepTime = 0
+        self.longestSleep = 0
+        self.longestMin = 0
+
+class Activity:
+
+    def __init__(self,act,timeStamp):
+        self.act = act
+        self.time = timeStamp
 
 if __name__ == '__main__':
     main()
